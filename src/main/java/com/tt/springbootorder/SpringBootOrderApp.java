9e8ea20589a998f66f461ed0fcb6982d7b77c047
xyf <905231404@qq.com>
2019-07-28 21:01:30 +0800

    删除web模块

--- src/main/java/com/tt/springbootorder/SpringBootOrderApp.java
+++ src/main/java/com/tt/springbootorder/SpringBootOrderApp.java
@@ -3,10 +3,18 @@ package com.tt.springbootorder;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan(value = "com.tt.springbootorder.dao")
-public class SpringBootOrderApp {
+public class SpringBootOrderApp extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return super.configure(builder);
+        return builder.sources(this.getClass());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootOrderApp.class, args);
