af3a77fecbcd27beb0a4d035b846ce23f5dd410b
xyf <905231404@qq.com>
2019-07-28 23:02:02 +0800

    修改了swagger的配置文件。

--- src/main/java/com/tt/springbootorder/config/swagger/WebMvcConfg.java
+++ src/main/java/com/tt/springbootorder/config/swagger/WebMvcConfg.java
@@ -8,19 +8,29 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 /**
  * @ClassName: com.tt.springbootorder.config.swagger.WebMvcConfg
  * @Description: mvc的配置
- * @Author:      Administrator
+ * @Author: Administrator
  * @CreateDate: 2019/7/28 0028 下午 10:47
- * @UpdateUser:   Administrator
- * @Version:        1.0
+ * @UpdateUser: Administrator
+ * @Version: 1.0
  **/
 @Configuration
 //public class WebMvcConfg extends WebMvcConfigurationSupport {
 public class WebMvcConfg extends WebMvcConfigurerAdapter {
 
-    @Override
+    /*@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
         registry.addResourceHandler("/**").addResourceLocations("classpath:/META-INF/resources/").setCachePeriod(0);
+
+    }*/
+
+    /**
+     * 增加swagger的支持
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 
     }
 }
